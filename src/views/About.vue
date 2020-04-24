028ace902ced8f3feb6355c4082b60148ffd9345
Kengo Wada <louiskengo3@gmail.com>
2020-04-24 22:43:09 +0300

    Updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
+  <div class="about text-center">
+    <vue-headful title="About | Noty" description="Noty about page" />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
