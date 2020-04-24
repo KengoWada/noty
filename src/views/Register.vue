02b5bb2b73f2f04c7b2a70caedd522d57a79afc7
Kengo Wada <louiskengo3@gmail.com>
2020-04-24 22:44:06 +0300

    Added register page

---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <vue-headful title="Register | Noty" description="Noty register page" />
+    <h1>Register Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register"
+};
+</script>
