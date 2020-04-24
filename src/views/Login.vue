dc2b58edde49b55777a30030d9ee11b5634ce7f1
Kengo Wada <louiskengo3@gmail.com>
2020-04-24 22:43:45 +0300

    Added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <vue-headful title="Log In | Noty" description="Noty log in page" />
+    <h1>Log In Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
