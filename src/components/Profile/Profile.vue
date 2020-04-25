b76fe68a6886bbffe23dc527dfa46553546b3a63
Kengo Wada <louiskengo3@gmail.com>
2020-04-25 08:44:01 +0300

    Added update profile button

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -2,8 +2,25 @@
   <div class="text-center">
     <vue-headful title="Profile | Noty" description="Noty profile page" />
     <h1>Profile</h1>
-    <button>
-      <router-link to="/profile/edit">Update Profile</router-link>
-    </button>
+    <div class="text-center mt-4">
+      <router-link class="btn white-border" to="/profile/edit" tag="button"
+        >Update Profile</router-link
+      >
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Profile"
+};
+</script>
+
+<style scoped>
+.white-border {
+  border: white 1px solid;
+}
+.white-border:hover {
+  color: white;
+}
+</style>
